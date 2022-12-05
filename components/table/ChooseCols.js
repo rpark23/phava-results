be0d72b809a1152ad68eeeed863e56e76292bf5d
Ryan Park <rpark@Ryans-MacBook-Pro-5.local>
2022-12-04 23:27:11 -0800

    Updated with runs from 12/4

--- components/table/ChooseCols.js
+++ components/table/ChooseCols.js
@@ -75,17 +75,19 @@ export default function ResultsTable (props) {
             <Switch checked={id} onChange={(e) => setID(e.currentTarget.checked)} label={<span><strong>ID</strong> - Unique identifier: &#123;genome name&#125;:&#123;IR indices&#125;</span>}/>
             <Switch checked={species} onChange={(e) => setSpecies(e.currentTarget.checked)} label={<span><strong>Species</strong> - Species that IR was found in</span>}/>
             <Switch checked={totalRuns} onChange={(e) => setTotalRuns(e.currentTarget.checked)} label={<span><strong>Total Runs</strong> - Total number of PhaVa runs for this taxa</span>}/>
-            <Switch checked={type1} onChange={(e) => setType1(e.currentTarget.checked)} label={<span><strong>Type 1</strong> - Type* of IR with respect to first appearing gene</span>}/>
-            <Switch checked={gb1} onChange={(e) => setGB1(e.currentTarget.checked)} label={<span><strong>Genbank 1</strong> - Genbank gene annotation (if available) for first appearing gene</span>}/>
-            <Switch checked={pfam1} onChange={(e) => setPfam1(e.currentTarget.checked)} label={<span><strong>Pfam 1</strong> - Pfam gene annotation (if available) for first appearing gene</span>}/>
+            {/* <Switch checked={type1} onChange={(e) => setType1(e.currentTarget.checked)} label={<span><strong>Type 1</strong> - Type* of IR with respect to first appearing gene</span>}/>
+            <Switch checked={gb1} onChange={(e) => setGB1(e.currentTarget.checked)} label={<span><strong>Genbank 1</strong> - Genbank gene annotation (if available) for first appearing gene</span>}/> */}
+            {/* <Switch checked={pfam1} onChange={(e) => setPfam1(e.currentTarget.checked)} label={<span><strong>Pfam 1</strong> - Pfam gene annotation (if available) for first appearing gene</span>}/> */}
           </div>
           <div className={styles.column}>
-          <Switch checked={type2} onChange={(e) => setType2(e.currentTarget.checked)} label={<span><strong>Type 2</strong> - Type* of IR (if present) with respect to second appearing gene</span>}/>
+            <Switch checked={type1} onChange={(e) => setType1(e.currentTarget.checked)} label={<span><strong>Type 1</strong> - Type* of IR with respect to first appearing gene</span>}/>
+            <Switch checked={gb1} onChange={(e) => setGB1(e.currentTarget.checked)} label={<span><strong>Genbank 1</strong> - Genbank gene annotation (if available) for first appearing gene</span>}/>
+            <Switch checked={type2} onChange={(e) => setType2(e.currentTarget.checked)} label={<span><strong>Type 2</strong> - Type* of IR (if present) with respect to second appearing gene</span>}/>
             <Switch checked={gb2} onChange={(e) => setGB2(e.currentTarget.checked)} label={<span><strong>Genbank 2</strong> - Genbank gene annotation (if available) for second appearing gene</span>}/>
-            <Switch checked={pfam2} onChange={(e) => setPfam2(e.currentTarget.checked)} label={<span><strong>Pfam 2</strong> - Pfam gene annotation (if available) for second appearing gene</span>}/>
+            {/* <Switch checked={pfam2} onChange={(e) => setPfam2(e.currentTarget.checked)} label={<span><strong>Pfam 2</strong> - Pfam gene annotation (if available) for second appearing gene</span>}/>
             <Switch checked={type3} onChange={(e) => setType3(e.currentTarget.checked)} label={<span><strong>Type 3</strong> - Type* of IR (if present) with respect to third appearing gene</span>}/>
             <Switch checked={gb3} onChange={(e) => setGB3(e.currentTarget.checked)} label={<span><strong>Genbank 3</strong> - Genbank gene annotation (if available) for third appearing gene</span>}/>
-            <Switch checked={pfam3} onChange={(e) => setPfam3(e.currentTarget.checked)} label={<span><strong>Pfam 3</strong> - Pfam gene annotation (if available) for third appearing gene</span>}/>
+            <Switch checked={pfam3} onChange={(e) => setPfam3(e.currentTarget.checked)} label={<span><strong>Pfam 3</strong> - Pfam gene annotation (if available) for third appearing gene</span>}/> */}
           </div>
         </div>
         <p style={{ fontSize: '0.8rem', marginTop: '2vh' }}>*Type can be either intragenic, partial (part of IR is within a gene), or intergenic</p>
